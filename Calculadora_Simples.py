9687e663ee2329c6c2dc991ae532aa59602ac27d
Mauricio Marques <mauriciomarquescosta7@gmail.com>
2023-05-29 18:53:04 -0300

    Adição de nova formatação de mensagem nas condições 'if' e 'elif', para ficar mais interativa a ação com o usuário. Adição da mensagem de agradecimento.

--- Calculadora_Simples.py
+++ Calculadora_Simples.py
@@ -1,25 +1,27 @@
 def calculadora_simples(num1, operador, num2):
 
     if operador == "+":
-        print("O seu resultado é",num1 + num2)
+        print(f"O resultado para {num1} {operador} {num2} =", num1 + num2)
 
     elif operador == "-":
-        print("O seu resultado é",num1 - num2)
+        print(f"O resultado para {num1} {operador} {num2} =",num1 - num2)
 
     elif operador == "*":
-        print("O seu resultado é",num1 * num2)
+        print(f"O resultado para {num1} {operador} {num2} =",num1 * num2)
 
     elif operador == "/":
-        print("O seu resultado é",num1 / num2)
+        print(f"O resultado para {num1} {operador} {num2} =",num1 / num2)
 
     #Caso seja inserido um número de operação que não exista, o resultado deverá ser 0.
 
     else:
         print("0")
 
+    print("Muito obrigado por utilizar a calculadora!")
+
 num1 = float (input("Digite o primeiro número: "))
 
-operador = str (input("Digite o operador lógico: adição = '+',subtração = '-', multiplicação = '*', divisão = '/' :"))
+operador = str (input("Digite o operador lógico: adição = '+',subtração = '-', multiplicação = '*', divisão = '/' : "))
 
 num2 = float (input("Digite o segundo número: "))
 
